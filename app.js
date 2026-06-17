60737282ce7a4fd2d4aadef49792530e5f97f1dc
OzenFern <ozenfernandes@gmail.com>
2026-06-17 19:14:19 +0530

    ref: add redirectWithMessage and fix routing bugs

--- app.js
+++ app.js
@@ -9,10 +9,14 @@ let postId = 0;
 
 /**
  * Creates a new post and automatically handles the id
- * @param {string} postBody - Metadata and contents of post
+ *
+ * @param {Object} postBody
+ * @param {string} postBody.title
+ * @param {string} postBody.blurb
+ * @param {string} postBody.content
  */
 function createPost({ title, blurb, content }) {
-  posts.push({ id: postId++, title: title, blurb: blurb, content: content });
+  posts.push({ id: postId++, title, blurb, content });
 }
 
 /**
@@ -35,6 +39,19 @@ function deletePost(id) {
 }
 
 /**
+ * Redirects to posts with action and title specified in the query
+ *
+ * @param {*} res - response
+ * @param {string} action - action performed
+ * @param {string} title - title of post
+ */
+function redirectWithMessage(res, action, title) {
+  res.redirect(
+    `/posts?action=${encodeURIComponent(action)}&title=${encodeURIComponent(title)}`,
+  );
+}
+
+/**
  * Renders a custom 404 page for 404 status code
  *
  * @param {*} res - response from the server
@@ -44,15 +61,25 @@ function render404(res) {
   return res.status(404).render("404.ejs");
 }
 
+/**
+ * Checks if the post exists and assigns it to request post
+ * Custom middleware
+ *
+ * @param {*} req
+ * @param {*} res
+ * @param {function} next
+ */
+function loadPost(req, res, next) {
+  const post = getPost(Number(req.params.id));
+  if (!post) return render404(res); // Throw a 404 error if post is not found
+  req.post = post;
+  next();
+}
+
 // Middleware
 app.use(express.static("public")); // Displays static files
 app.use(express.urlencoded({ extended: true })); // Parses user data
 app.use(morgan("dev")); // Logs HTTP requests
-
-// Sets up server at specified port
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:3000`);
-});
 
 // Renders the homepage
 app.get("/", (req, res) => {
@@ -62,7 +89,7 @@ app.get("/", (req, res) => {
 // Displays all posts
 app.get("/posts", (req, res) => {
   res.render("posts.ejs", {
-    blogs: posts,
+    posts,
     title: req.query.title,
     action: req.query.action,
   });
@@ -77,52 +104,40 @@ app.get("/posts/new", (req, res) => {
 app.post("/posts/create", (req, res) => {
   const post = req.body;
   createPost(post);
-  res.redirect(`/posts?action=created&title=${encodeURIComponent(post.title)}`);
+
+  redirectWithMessage(res, "created", post.title);
 });
 
 // Displays selected post
-app.get("/posts/:id", (req, res) => {
-  const post = getPost(Number(req.params.id));
-  if (!post) {
-    return render404(res);
-  } // Throw a 404 error if post is not found
+app.get("/posts/:id", loadPost, (req, res) => {
   res.render("post.ejs", {
-    post,
+    post: req.post,
   });
 });
 
 // Display a page for editing post
-app.get("/posts/:id/edit", (req, res) => {
-  const post = getPost(Number(req.params.id));
-  if (!post) {
-    return render404(res);
-  }
+app.get("/posts/:id/edit", loadPost, (req, res) => {
   res.render("edit.ejs", {
-    post,
+    post: req.post,
   });
 });
 
 // Handles editing of the post
-app.post("/posts/:id/edit", (req, res) => {
+app.post("/posts/:id/edit", loadPost, (req, res) => {
   const post = getPost(Number(req.params.id));
   // Update the post with the values entered by the user
-  post.title = req.body.title;
-  post.blurb = req.body.blurb;
-  post.content = req.body.content;
+  req.post.title = req.body.title;
+  req.post.blurb = req.body.blurb;
+  req.post.content = req.body.content;
 
-  res.redirect(`/posts?action=edited&title=${encodeURIComponent(post.title)}`);
+  redirectWithMessage(res, "edited", post.title);
 });
 
 // Handles deletion of a post
-app.get("/posts/:id/delete", (req, res) => {
-  const id = Number(req.params.id); // Explicitly convert id to a number
-  const post = getPost(id);
-  if (!post) {
-    return render404(res);
-  } // Throw a 404 error if post is not found
+app.get("/posts/:id/delete", loadPost, (req, res) => {
+  deletePost(req.post.id);
 
-  deletePost(id);
-  res.redirect(`/posts?action=deleted&title=${encodeURIComponent(post.title)}`);
+  redirectWithMessage(res, "deleted", req.post.title);
 });
 
 app.use((req, res) => {
@@ -159,4 +174,9 @@ createPost({
 This blog exists as a hands-on project to practice backend development, routing, and templating with EJS.
 
 Each feature added here represents a small step toward understanding full-stack development better.`,
+});
+
+// Sets up server at specified port
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:3000`);
 });
