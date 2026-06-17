335b226eb3e469ca81c9f37143e5371081b92e47
OzenFern <ozenfernandes@gmail.com>
2026-06-18 01:48:12 +0530

    Update port to dynamically serve a web port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 import morgan from "morgan";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Use PORT on web server if available
 
 let posts = [];
 let postId = 0;
