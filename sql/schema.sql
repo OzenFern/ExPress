5629446434cfe9b10f24d9f56e7d6d81f4494093
OzenFern <ozenfernandes@gmail.com>
2026-09-05 15:58:24 +0530

    Fix the syntax of schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,16 +1,14 @@
--- Users Table --
 CREATE TABLE users (
-    user_id SERIAL PRIMARY KEY
+    user_id SERIAL PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
-    password TEXT NOT NULL
-    created_at TIMESTAMPZ DEFAULT CURRENT_TIMESTTAMP
+    password TEXT NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
--- Posts Table --
 CREATE TABLE posts (
     post_id SERIAL PRIMARY KEY,
-    title VARCHAR(100) NOT NULL CHECK(LENGTH(title)>=3),
-    blurb VARCHAR(180) CHECK(LENGTH(blurb)>=10),
+    title VARCHAR(100) NOT NULL CHECK (LENGTH(title) >= 3),
+    blurb VARCHAR(180) CHECK (LENGTH(blurb) >= 10),
     content TEXT,
-    user TEXT REFERENCES users(user_id) ON DELETE CASCADE
+    user_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE
 );
