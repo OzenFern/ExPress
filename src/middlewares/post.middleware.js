947318fcfde9433de2901740a0a6fd9f126bb279
OzenFern <ozenfernandes@gmail.com>
2026-09-05 21:31:12 +0530

    Rename id to user_id for better clarity

--- src/middlewares/post.middleware.js
+++ src/middlewares/post.middleware.js
@@ -2,7 +2,7 @@ import { getPost } from "../services/post.service.js";
 import { notFound } from "./notFound.middleware.js";
 
 export const loadPost = async (req, res, next) => {
-  const post = await getPost(Number(req.params.id), req.user.id);
+  const post = await getPost(Number(req.params.id), req.user.user_id);
 
   if (!post) return notFound(req, res);
 
