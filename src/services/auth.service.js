dbb433b2bb691db475f9d26e468ad90a2038b204
OzenFern <ozenfernandes@gmail.com>
2026-09-05 13:08:00 +0530

    Wrap logic into asyncTryCatch

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,12 +1,9 @@
 import bcrypt from "bcryptjs";
 import { createUser } from "../repositories/user.repositories.js";
+import { asyncTryCatch } from "../utils/error.utils.js";
 
 const saltRounds = 12;
 
-export async function registerUser(username, password) {
-  try {
-    return await createUser(username, bcrypt.hash(password, saltRounds));
-  } catch (err) {
-    console.error(err.stack);
-  }
-}
+export const registerUser = asyncTryCatch((username, password) =>
+  createUser(username, bcrypt.hash(password, saltRounds)),
+);
