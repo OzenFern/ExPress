3ba50526cedd6a2be48fc5cee4c8924d733ea0dd
OzenFern <ozenfernandes@gmail.com>
2026-09-05 19:52:26 +0530

    Add ssl config for neon connection

--- src/db/pool.js
+++ src/db/pool.js
@@ -3,6 +3,9 @@ import env from "../config/env.config.js";
 
 const pool = new pg.Pool({
   connectionString: env.dbConnection,
+  ssl: env.dbConnection?.includes("neon.tech")
+    ? { rejectUnauthorized: false }
+    : undefined,
 });
 
 export default pool;
