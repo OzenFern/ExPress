4356bae4a3207fe86fc7b802cfe1176b9a53f246
OzenFern <ozenfernandes@gmail.com>
2026-09-05 15:50:12 +0530

    Format code with prettier

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,19 +1,19 @@
-import {Router} from "express";
-import {requireAuth} from "../middlewares/auth.middleware.js";
+import { Router } from "express";
+import { requireAuth } from "../middlewares/auth.middleware.js";
 import {
-    createNewPost,
-    getSinglePost,
-    getPosts,
-    renderCreatePostForm,
-    deleteSinglePost,
-    editSinglePost, renderEditPostForm
+  createNewPost,
+  getSinglePost,
+  getPosts,
+  renderCreatePostForm,
+  deleteSinglePost,
+  editSinglePost,
+  renderEditPostForm,
 } from "../controllers/post.controller.js";
-import {loadPost} from "../middlewares/post.middleware.js";
-
+import { loadPost } from "../middlewares/post.middleware.js";
 
 const router = Router();
 
-router.use(requireAuth)
+router.use(requireAuth);
 
 router.get("/", getPosts);
 router.get("/new", renderCreatePostForm);
