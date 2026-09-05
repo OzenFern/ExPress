34c7394a2e2de9f73a348eca3c54028a1a600047
OzenFern <ozenfernandes@gmail.com>
2026-09-05 21:29:42 +0530

    Match column names & add allowedFields check

--- src/repositories/post.repositories.js
+++ src/repositories/post.repositories.js
@@ -43,7 +43,7 @@ export async function getPost(postId, userId) {
  */
 export async function createPost(post, userId) {
   const { rows } = await pool.query(
-    "INSERT INTO posts (title, blurb, content, user) VALUES ($1, $2, $3, $4) RETURNING *",
+    "INSERT INTO posts (title, blurb, content, user_id) VALUES ($1, $2, $3, $4) RETURNING *",
     [post.title, post.blurb, post.content, userId],
   );
 
@@ -61,9 +61,15 @@ export async function createPost(post, userId) {
  * @param {*} userId
  */
 export async function updatePost(postId, post, userId) {
-  const fields = Object.keys(post);
-  const values = Object.values(post);
-  const setClause = fields.map((field, index) => `${field}=${index + 1}`);
+  const allowedFields = ["title", "blurb", "content"];
+  const fields = Object.keys(post).filter((field) =>
+    allowedFields.includes(field),
+  );
+  const values = fields.map((field) => post[field]);
+
+  if (fields.length === 0) return getPost(postId, userId);
+
+  const setClause = fields.map((field, index) => `${field}=$${index + 1}`);
 
   //   Add post_id & user_id at the back of values
   values.push(postId, userId);
