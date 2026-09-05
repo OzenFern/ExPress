fcbdcaac5654db0c36e850d68108eace02c65a3b
OzenFern <ozenfernandes@gmail.com>
2026-09-05 13:07:15 +0530

    Remove the redundant async in outer function

--- src/utils/error.utils.js
+++ src/utils/error.utils.js
@@ -1,11 +1,11 @@
-export async function asyncTryCatch(fn, fallbackValue = null) {
-    return async (...args) => {
-        try {
-            return await fn(...args);
-        } catch (err) {
-            console.error(`Async Error in ${fn.name || 'anonymous'}: ${err.message}`);
-            console.error(err.stack);
-            return fallbackValue;
-        }
+export function asyncTryCatch(fn, fallbackValue = null) {
+  return async (...args) => {
+    try {
+      return await fn(...args);
+    } catch (err) {
+      console.error(`Async Error in ${fn.name || "anonymous"}: ${err.message}`);
+      console.error(err.stack);
+      return fallbackValue;
     }
+  }
 }
