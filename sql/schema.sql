76df40b6b246c32bf57e71ad8117487b37c86b10
OzenFern <ozenfernandes@gmail.com>
2026-09-05 16:08:10 +0530

    Add comments

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,3 +1,4 @@
+-- users schema --
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
@@ -5,6 +6,7 @@ CREATE TABLE users (
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
+-- posts schema --
 CREATE TABLE posts (
     post_id SERIAL PRIMARY KEY,
     title VARCHAR(100) NOT NULL CHECK (LENGTH(title) >= 3),
