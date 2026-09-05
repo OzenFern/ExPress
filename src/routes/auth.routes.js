fe7f8d015bc11f9168a1efe3619f0be634abccd4
OzenFern <ozenfernandes@gmail.com>
2026-09-05 18:50:06 +0530

    Add caching to routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -6,11 +6,12 @@ import {
   showLogin,
   showRegister,
 } from "../controllers/auth.controller.js";
+import { cache } from "../middlewares/cache.middleware.js";
 
 const router = Router();
 
-router.get("/register", showRegister);
-router.get("/login", showLogin);
+router.get("/register", cache(3600), showRegister);
+router.get("/login", cache(3600), showLogin);
 
 router.post("/register", register);
 
--- src/routes/page.routes.js
+++ src/routes/page.routes.js
@@ -3,10 +3,11 @@ import {
   renderAboutPage,
   renderHomePage,
 } from "../controllers/page.controller.js";
+import { cache } from "../middlewares/cache.middleware.js";
 
 const router = new Router();
 
-router.get("/", renderHomePage);
-router.get("/about", renderAboutPage);
+router.get("/", cache(3600), renderHomePage);
+router.get("/about", cache(86400), renderAboutPage);
 
 export default router;
